50a3c122f5bb48823443338ea690b401b17fa3d7
Tiago Santos <tiagogs98@outlook.com>
2022-05-04 15:32:55 -0300

    Adiciona funcionalidades de inserir nickname e recuperar no lado servidor, exibir junto a mensagem enviada com hora e minuto o nickname inserido na página.

--- public/js/index.js
+++ public/js/index.js
@@ -4,26 +4,71 @@ let messages = document.querySelector("#messages");
 let form = document.querySelector("#form");
 let input = document.querySelector("#input");
 
+let modal = document.querySelector("#modal-container");
+let userForm = document.querySelector("#user-form");
+let nicknameInput = document.querySelector("#nickname-input");
+
+let LoggedAs;
+
+let userList = [];
+let messageList = [];
+
 form.addEventListener('submit', (event) => {
     event.preventDefault();
     if(input.value) {
-        socket.emit("chat message", input.value);
+        socket.emit("chat message", {author: LoggedAs, msg: input.value});
         input.value = '';
     }
 });
 
-socket.on("chat message", (msg) => {
-    addNewLineOnChat(msg);
-    
+userForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+    if(nicknameInput.value) {
+        socket.emit("new nickname", nicknameInput.value);
+        LoggedAs = nicknameInput.value;
+        nicknameInput.value = '';
+    }
 });
 
-socket.on("chat event", (msg) => {
-    addNewLineOnChat(msg);
+socket.on("chat message", (data) => {
+    addNewLineOnChat(data);
+});
+
+socket.on("chat event", (data) => {
+    addNewLineOnChat(data);
 })
+
+socket.on("new nickname", (nickname) => {
+    handleNewNickname(nickname)
+});
+
+socket.on("message list", (data) => {
+    messageList = data.messageList;
+    renderMessageList();
+})
+
+function renderMessageList() {
+    messageList.map(message => {
+        addNewLineOnChat(message);
+    });
+}
 
 function addNewLineOnChat(content) {
     let item = document.createElement('li');
-    item.textContent = content;
+    const d = new Date(content.createdAt);
+    const month = d.getMonth();
+    const day = d.getDate();
+    const h = d.getHours();
+    const m = d.getMinutes();
+
+    const now = `${h}:${m}`;
+    
+    item.textContent = content.author ? `${content.author}: ${content.msg} - ${now}` : content.messages;
     messages.appendChild(item);
     window.scrollTo(0, document.body.scrollHeight);
+}
+
+function handleNewNickname(nickname) {
+    // modal.hidden = true;
+    userList.push(nickname);
 }
--- src/websocket.js
+++ src/websocket.js
@@ -1,13 +1,65 @@
+const { Server } = require("socket.io");
 const { io } = require("./http");
+
+class User {
+    id;
+    nickname;
+    constructor(id, nickname) {
+        this.id = id;
+        this.nickname = nickname;
+    }
+}
+
+let userList = [];
+let messageList = [];
 
 io.on("connection", (socket) => {
     socket.broadcast.emit("chat event", "someone conected");
 
+    socket.emit("message list", {messageList});
+
     socket.on("disconnect", () => {
-        socket.broadcast.emit("chat event", "someone disconected");
+        const outUser = userList.find(user => user.id == socket.id);
+
+        if(outUser) {
+            socket.broadcast.emit("chat event", `${outUser.nickname} disconected`);
+            const index = userList.indexOf(outUser);
+            userList.slice(index, 1);
+        }
     });
 
-    socket.on("chat message", (msg) => {
-        io.emit("chat message", msg);
+    socket.on("chat message", (data) => {
+        const d = new Date();
+
+        data.createdAt = d;
+        
+        
+        if(userList.find(user => user.nickname == data.author)) {
+            io.emit("chat message", {author: data.author, msg: data.msg, createdAt: data.createdAt});
+            messageList.push(data);
+        } else {
+            data = {
+                author: 'Server',
+                msg: 'Nickname não registrado corretamente.',
+                createdAt: d
+            };
+            messageList.push(data);
+            socket.emit('chat message', data);            
+        }
+    })
+
+    socket.on("new nickname", (nickname) => {
+        socket.emit("new nickname", nickname);
+
+        let userExists = userList.find(user => user.nickname == nickname);
+
+        if(userExists) {
+            let index = userList.indexOf(userExists);
+            userExists.id = socket.id;
+            userList[index] = userExists;
+        } else {
+            const user = new User(socket.id, nickname);
+            userList.push(user);
+        }
     })
 });
